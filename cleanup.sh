a007675d2639971d841cecbad1f22dbe68c1a8bf
cilegordev <deputyalectro@icloud.com>
2025-04-05 15:56:24 +0700

    Add AIK

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# AIK-Linux/cleanup: reset working directory
+# osm0sis @ xda-developers
+
+cd "$PWD";
+rm -rf ramdisk split_img boot.img new.* *.gz;
+echo "Working directory cleaned.";
+return 0;
+
